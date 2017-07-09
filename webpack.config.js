323df5c134fa53e4e066104132008af0d812bb87
Francisco Diniz <hediniz@icloud.com>
2017-07-09 17:48:17 -0300

    [update] webpack basic configs

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,40 @@
  * Created by hediniz on 7/7/17.
  */
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const src_path = path.resolve(__dirname, './src');
+const dist_path = path.resolve(__dirname, './dist');
+
+const index_html = new HtmlWebpackPlugin({
+    template: 'index.template.html',
+    filename: 'index.html',
+});
+
+const babel_loader = {
+    test: /\.(js|es6)$/,
+    exclude: [/node_modules/],
+    use: [{
+        loader: 'babel-loader',
+        options: { presets: ['es2015', 'stage-2'] },
+    }],
+};
 
 module.exports = {
-    entry: './src/assets/main.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    }
+    resolve: { extensions: ['.js', '.es6'] },
+    context: src_path,
+
+    output: { filename: 'js/[name].js', path: dist_path },
+
+    entry: {
+        main: './assets/main.js'
+    },
+
+    plugins: [
+      index_html
+    ],
+
+    module: { rules: [
+        babel_loader
+    ] },
 };
