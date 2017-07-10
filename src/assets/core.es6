c606060e17ec9836f0ebfa0a2909f99954656357
Francisco Diniz <hediniz@icloud.com>
2017-07-10 18:20:52 -0300

    load and insert table content on click

--- src/assets/core.es6
+++ src/assets/core.es6
@@ -10,19 +10,24 @@ function handleFile(event) {
     reader.onload = (function(file) {
         return function (e) {
             // read and render
-            $('#content').load(e.target.result);
+            $('#insert-html').load(e.target.result);
         };
     })(file);
     reader.readAsDataURL(file);
-
 }
 
 function handleInvoiceHtml(){
+
+    const html = $('.box-expansivel').html();
+    
+    $('#insert-extraction').append(html);
+    $( '#insert-html' ).remove();
+
     const data = document.getElementsByClassName('rowNormal');
 
     let newTable = [];
 
-    for (let row of data) {
+    for ( let row of data ) {
 
         let transaction = [];
         let transactionData = [];
@@ -34,11 +39,46 @@ function handleInvoiceHtml(){
         transaction.push(transactionData);
         newTable.push(transaction);
     }
-    console.log(newTable);
+    tableFilling(newTable);
 }
 
 function tableFilling(tableData) {
-    console.log(tableData);
+
+    $('#insert-table').append( () => {
+        return `<table class='table table-striped jambo_table bulk_action'>
+             <thead>
+                <tr class='headings'>
+                <th>
+                    <input type='checkbox' id='check-all' class='flat'>
+                </th>
+                <th class='column-title'>Date </th>
+                <th class='column-title'>Transaction </th>
+                <th class='column-title'>US$ </th>
+                <th class='column-title last'>R$ </th>
+                <th class='bulk-actions' colspan='7'>
+                    <a class='antoo' style='color:#fff; font-weight:500;'>Bulk Actions ( <span class='action-cnt'> </span> ) <i class='fa fa-chevron-down'></i></a>
+                </th>
+                </tr>
+            </thead>
+            <tbody id='insert-row'>
+    
+            </tbody>
+        </table>`;
+    });
+
+    for ( let row of tableData ) {
+
+        $('#insert-row').append( () => {
+            return  `<tr class=' ' >
+                        <td class='a-center '> <input type='checkbox' class='flat' name='table_records'> </td>
+                        <td class=''> ${row[0][0]} </td>
+                        <td class=''> ${row[0][1]} </td>
+                        <td class=''> ${row[0][2]} </td>
+                        <td class=''> ${row[0][3]} </td>
+                    </tr>`;
+           }
+        );
+    }
 }
 
 export { handleFile, handleInvoiceHtml };
--- src/assets/main.js
+++ src/assets/main.js
@@ -3,11 +3,76 @@
  */
 import * as core from './core.es6';
 
-// const ele = document.getElementById;
-let inputElement = document.getElementById("invoice");
-inputElement.addEventListener('change', core.handleFile, false);
+$('#invoice').change( (event) => {
+    return core.handleFile(event);
+});
 
 $('#read-html').click( () => {
-    return core.handleInvoiceHtml();
+
+    core.handleInvoiceHtml();
+    // iCheck
+    $(document).ready(function() {
+        if ($("input.flat")[0]) {
+            $(document).ready(function () {
+                $('input.flat').iCheck({
+                    checkboxClass: 'icheckbox_flat-green',
+                    radioClass: 'iradio_flat-green'
+                });
+            });
+        }
+    });
+
+    // Table
+    let checkState = '';
+
+    $('table input').on('ifChecked', function () {
+        checkState = '';
+        $(this).parent().parent().parent().addClass('selected');
+        countChecked();
+    });
+    $('table input').on('ifUnchecked', function () {
+        checkState = '';
+        $(this).parent().parent().parent().removeClass('selected');
+        countChecked();
+    });
+
+    $('.bulk_action input').on('ifChecked', function () {
+        checkState = '';
+        $(this).parent().parent().parent().addClass('selected');
+        countChecked();
+    });
+    $('.bulk_action input').on('ifUnchecked', function () {
+        checkState = '';
+        $(this).parent().parent().parent().removeClass('selected');
+        countChecked();
+    });
+    $('.bulk_action input#check-all').on('ifChecked', function () {
+        checkState = 'all';
+        countChecked();
+    });
+    $('.bulk_action input#check-all').on('ifUnchecked', function () {
+        checkState = 'none';
+        countChecked();
+    });
+
+    function countChecked() {
+        if (checkState === 'all') {
+            $(".bulk_action input[name='table_records']").iCheck('check');
+        }
+        if (checkState === 'none') {
+            $(".bulk_action input[name='table_records']").iCheck('uncheck');
+        }
+
+        let checkCount = $(".bulk_action input[name='table_records']:checked").length;
+
+        if (checkCount) {
+            $('.column-title').hide();
+            $('.bulk-actions').show();
+            $('.action-cnt').html(checkCount + ' Records Selected');
+        } else {
+            $('.column-title').show();
+            $('.bulk-actions').hide();
+        }
     }
-);
+});
+
