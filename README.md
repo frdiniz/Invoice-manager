f0caa7a20f3570f53238283b25c5d2cb919e7223
Francisco Diniz <hediniz@icloud.com>
2017-07-16 14:52:57 -0300

    [update] info update

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Invoice-manager
 
-Tool import html invoice from bradesco cards, select desired items and export .csv
+Personal tool to import html invoice, select desired items and export .csv
+
+## Installation
+8	
+9	* `git clone https://github.com/frdiniz/Invoice-manager.git`
+10	* `cd Invoice-manager` (OSX, Linux and Windows)
+11	* `npm install`
--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,48 @@
  * Created by hediniz on 7/7/17.
  */
 const path = require('path');
+const webpack = require('webpack');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const src_path = path.resolve(__dirname, './src');
 const dist_path = path.resolve(__dirname, './dist');
+
+const vendors_chunk = new webpack.optimize.CommonsChunkPlugin({
+    name: 'vendors',
+    minChunks: ({ context }) => (
+        // this assumes your vendor imports exist in the node_modules directory
+        context && context.indexOf('node_modules') !== -1
+    ),
+});
+
+
+/*
+ * CommonChunksPlugin will now extract all the common modules from vendor and main bundles
+ * But since there are no more common modules between them we end up with just the runtime code included in the manifest file
+ */
+
+const manifest_chunk = new webpack.optimize.CommonsChunkPlugin({
+    name: 'manifest',
+});
+
+const extract_css_plugin = new ExtractTextPlugin({
+    filename: 'css/[name].[chunkhash:8].css',
+});
 
 const index_html = new HtmlWebpackPlugin({
     template: 'index.template.html',
     filename: 'index.html',
 });
+
+const css_loader = {
+    test: /\.css$/,
+    use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: 'css-loader',
+    }),
+};
 
 const babel_loader = {
     test: /\.(js|es6)$/,
@@ -22,20 +55,27 @@ const babel_loader = {
 };
 
 module.exports = {
-    resolve: { extensions: ['.js', '.es6'] },
+    resolve: { extensions: ['.js', '.es6', '.css'] },
     context: src_path,
 
-    output: { filename: 'js/[name].js', path: dist_path },
+    output: {
+        filename: 'js/[name].[chunkhash:8].js', path: dist_path,
+    },
 
     entry: {
-        main: './assets/controller.js'
+        main: './assets/javascript/controller.js',
+        custom: './assets/stylesheets/custom.css',
     },
 
     plugins: [
-      index_html
+        vendors_chunk,
+        manifest_chunk,
+        extract_css_plugin,
+        index_html,
     ],
 
     module: { rules: [
-        babel_loader
+        babel_loader,
+        css_loader,
     ] },
 };
